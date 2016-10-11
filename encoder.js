0d2ec1249aa23396fe3b6e888833b759bfcab141
Owen Vandevelde <owen.vnade@gmail.com>
2016-10-11 11:30:33 -0700

    radio and alphabet

--- encoder.js
+++ encoder.js
@@ -1,5 +1,39 @@
 document.getElementById("inputArea").addEventListener("keydown",function(e){
 
-document.getElementById("textArea").innerHTML += document.getElementById("inputText").value;
+  var alphabet = {
+    65: "a",
+    66: "b",
+    67: "c",
+    68: "d",
+    69: "e",
+    70: "f",
+    71: "g",
+    72: "h",
+    73: "i",
+    74: "j",
+    75: "k",
+    76: "l",
+    77: "m",
+    78: "n",
+    79: "o",
+    80: "p",
+    81: "q",
+    82: "r",
+    83: "s",
+    84: "t",
+    85: "u",
+    86: "v",
+    87: "w",
+    88: "x",
+    89: "y",
+    90: "z",
+    8: "",
+    32: " ",
+  }
 
+  document.getElementById("textArea").innerHTML += alphabet[e.keyCode];
 })
+
+function run1(){
+  document.getElementById("textArea").innerHTML = "";
+}
