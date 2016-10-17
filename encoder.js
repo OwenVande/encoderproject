0a63fcab44458991121972c5be921425e8e7375c
Owen Vandevelde <owen.vnade@gmail.com>
2016-10-17 11:38:31 -0700

    attempt at decoder

--- encoder.js
+++ encoder.js
@@ -154,3 +154,43 @@ $("input[type='radio']").click(function(e){
 function run1(){
   document.getElementById("textArea").innerHTML = "";
 }
+
+document.getElementById("translationInputArea").addEventListener("keydown",function(d){
+
+  if (d.keyCode == 8){ //backspace
+    document.getElementById('translationArea').textContent = document.getElementById('translationArea').textContent.substr(0, document.getElementById('translationArea').textContent.length - 1);
+  }
+  var decoder = {
+    65: "a",
+    66: "b",
+    67: "c",
+    68: "d",
+    69: "e",
+    70: "f",
+    71: "g",
+    72: "h",
+    73: "i",
+    74: "j",
+    75: "k",
+    76: "l",
+    77: "m",
+    78: "n",
+    79: "o",
+    80: "p",
+    81: "q",
+    82: "r",
+    83: "s",
+    84: "t",
+    85: "u",
+    86: "v",
+    87: "w",
+    88: "x",
+    89: "y",
+    90: "z",
+    //8: "",
+    32: " ",
+  }
+
+  document.getElementById("translationArea").innerHTML += decoder[d.keyCode];
+
+});
